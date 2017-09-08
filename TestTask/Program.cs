db5e96aa082a4b434627fa5fef57ef2b5fc2ddf2
Goryansky Roman <rgoryansky@gmail.com>
2017-09-08 11:21:21 +0800

    Add a full code

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Threading;
 
 namespace TestTask
 {
@@ -10,6 +13,20 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
+            Words w = new Words("dictionary.txt");
+            Console.Write("Please, enter your word: ");
+            string request = Console.ReadLine();
+            if (request == "###")
+            {
+                Console.Write("Application is over");
+                Thread.Sleep(2000);
+                return;
+            }
+            else
+            {
+                w.Complete(request);
+            }
+            Console.ReadKey(true);
         }
     }
 }
---
+++ TestTask/Words.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestTask
+{
+    class Words
+    {
+        public SortedSet<string> sortedWords;
+
+        public Words(string path)
+        {
+            string[] readedwords = File.ReadAllLines(path, Encoding.GetEncoding(1251));
+            sortedWords = new SortedSet<string>(readedwords);
+        }
+        public void Complete(string request)
+        {
+            int counts = 1;
+            foreach (string word in sortedWords)
+                if (word.StartsWith(request) && counts <= 5)
+                {
+                    Console.WriteLine(word);
+                    ++counts;
+                }
+            if (counts == 1)
+                Console.WriteLine("No matches found");
+        }
+    }
+}
